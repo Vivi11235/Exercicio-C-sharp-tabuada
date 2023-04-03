31c9774bd33b2cc51da33580f53c1b85c64d4dff
Victoria <114111252+Vivi11235@users.noreply.github.com>
2023-04-03 03:12:23 +0000

    Cálculo da tabuada

--- main.cs
+++ main.cs
@@ -2,6 +2,12 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Informe um número para calcular a tabuada: ");
+    int n = int.Parse(Console.ReadLine());
+
+    for (int volta=0; volta<=10; volta++){
+      int x = n * volta;
+      Console.WriteLine("{0} x {1} = {2}", n, volta, x);
+    }
   }
 }
